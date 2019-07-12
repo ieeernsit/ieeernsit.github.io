1c4716d62b82836f40250891f08857354a9fe99e
Aishwarya Karanth <a.karanth934@gmail.com>
2019-07-12 19:49:09 +0530

    IEEE RNSIT Website

---
+++ index.js
@@ -0,0 +1,14 @@
+var exp = document.getElementsByClassName("expand");
+var i;
+
+for (i = 0; i < exp.length; i++) {
+  exp[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
