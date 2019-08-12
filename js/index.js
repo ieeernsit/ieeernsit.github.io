210e105f275d94e930978efc444f90b66fc5e8d3
Aishwarya Karanth <a.karanth934@gmail.com>
2019-08-12 16:57:19 +0530

    nav bar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+function openNav(){
+  	document.getElementById("mySidebar").style.width="250px" ;
+  	document.getElementById("main").style.marginLeft= "250px";
+  }
+  function closeNav(){
+  	document.getElementById("mySidebar").style.width="0" ;
+  	document.getElementById("main").style.marginLeft="0" ;
+
+  }
+
 $(function(){
 
   window.sr = ScrollReveal();
@@ -32,6 +42,16 @@ $(function(){
 	  });
 
   }
+  /*function openNav(){
+  	document.getElementById("hide-nav").style.width="25px" ;
+  	document.getElementById("main").style.marginLeft= "25px";
+  }
+  function closeNav(){
+  	document.getElementById("hide-nav").style.width="0" ;
+  	document.getElementById("main").style.marginLeft="0" ;
+
+  }
+	*/
   
   /*sr.reveal('.js--fadeInLeft', {
 	    origin: 'left',
