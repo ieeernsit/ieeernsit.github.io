9682968db885925ff6abd3f0070adebd7f342162
Aishwarya Karanth <a.karanth934@gmail.com>
2019-08-23 17:30:26 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-function openNav(){
-  	document.getElementById("mySidebar").style.width="250px" ;
-  	document.getElementById("main").style.marginLeft= "250px";
-  }
-  function closeNav(){
+function closeNav(){
   	document.getElementById("mySidebar").style.width="0" ;
   	document.getElementById("main").style.marginLeft="0" ;
 
+  }
+ function openNav(){
+  	document.getElementById("mySidebar").style.width="250px" ;
+  	document.getElementById("main").style.marginLeft= "250px";
   }
 
 $(function(){
