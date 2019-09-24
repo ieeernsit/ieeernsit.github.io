96980ab43aae77d3f42239fbe0dc57a231077e06
Aishwarya Karanth <a.karanth934@gmail.com>
2019-09-24 23:12:45 +0530

    update 2

--- js/index.js
+++ js/index.js
@@ -7,6 +7,16 @@ function closeNav(){
   	document.getElementById("mySidebar").style.width="250px" ;
   	document.getElementById("main").style.marginLeft= "250px";
   }
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 $(function(){
 
