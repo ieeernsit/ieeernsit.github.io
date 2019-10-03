c82a213cafc64f044deb447c2dcb34d6f6a06345
Aishwarya Karanth <a.karanth934@gmail.com>
2019-10-03 22:16:59 +0530

    python workshop update

--- js/index.js
+++ js/index.js
@@ -7,20 +7,17 @@ function closeNav(){
   	document.getElementById("mySidebar").style.width="250px" ;
   	document.getElementById("main").style.marginLeft= "250px";
   }
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
 
+ 
+
+function headerUp(){
+	window.scrollby()
+}
 $(function(){
 
   window.sr = ScrollReveal();
+
+  
 
   if ($(window).width() < 768) {
 
